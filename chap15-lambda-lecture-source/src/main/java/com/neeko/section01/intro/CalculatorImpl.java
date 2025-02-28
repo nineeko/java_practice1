849cad85d9b25e4109e6050909961c540ba5d98b
nineeko <gkxm1013@naver.com>
2025-02-28 09:40:40 +0900

    25.02.28 09:39

---
+++ chap15-lambda-lecture-source/src/main/java/com/neeko/section01/intro/CalculatorImpl.java
@@ -0,0 +1,9 @@
+package com.neeko.section01.intro;
+
+public class CalculatorImpl implements Calculator {
+
+    @Override
+    public int sumTwoNumber(int a, int b) {
+        return a + b;
+    }
+}
